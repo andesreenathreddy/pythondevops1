c5f5b7853993c3466f3fadda4939372efcf137e9
andesreenathreddy <andesreenathreddy@gmail.com>
2022-01-24 22:22:28 +0530

    api URL is chaged

--- productsapi.py
+++ productsapi.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 # http://localhost:3000/api/products
-@app.route('/api/products')  # this is decorator
+@app.route('/api/v2/products')  # this is decorator
 def productList():
     return jsonify(products)
 
